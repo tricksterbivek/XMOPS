f48255f3982f9c4f640a810265da27580868dc92
tricksterbivek <tricksterbivek@gmail.com>
2024-01-30 12:38:55 +1100

    Created the react app and added the login Page

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+import LoginPage from './components/Login/LoginPage'
+
+function App() {
+  return (
+    <div className="App">
+      <LoginPage />
+      {/* Other components and routes */}
+    </div>
+  );
+}
+
+export default App;
