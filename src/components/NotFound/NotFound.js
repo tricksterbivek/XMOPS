07268cd7f1b3909edbb7c7557b7f6e61572afd7f
tricksterbivek <tricksterbivek@gmail.com>
2024-02-02 15:32:31 +1100

    Correct the routes signup to verify && login to dashboard

---
+++ src/components/NotFound/NotFound.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+const NotFound = () => {
+  return (
+    <div>
+      <h2>NotFound</h2>
+      {/* NotFound content goes here */}
+    </div>
+  );
+};
+
+export default NotFound;
