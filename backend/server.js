f9f0a750e21d42cbb242c9334a1d0df102832d46
tricksterbivek <tricksterbivek@gmail.com>
2024-02-11 20:36:27 +1100

    added wp to the backend

--- backend/server.js
+++ backend/server.js
@@ -6,36 +6,43 @@ const PORT = process.env.PORT || 3001;
 
 app.use(express.static(path.join(__dirname, '..', 'build')));
 
-// app.get('/ping', (req, res) => {
-//   return res.send('pong');
-// });
-
-// app.post('/api/action', (req, res) => {
-//   console.log('Action button was clicked!');
-//   res.json({ message: 'Action was successfully triggered' });
-// });
 
 
-app.post('/api/deployEC2', (req, res) => {
+
+app.post('/api/deployWP', (req, res) => {
+  
   exec('terraform apply -auto-approve', { cwd: '../terraform/' }, (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`);
       return res.status(500).send({ message: 'Error executing Terraform apply' });
     }
     console.log(`stdout: ${stdout}`);
-    res.send({ message: 'EC2 Instance deployed successfully' });
+  
+    exec('terraform output -json', { cwd: '../terraform/' }, (error, outputStdout, stderr) => {
+      if (error) {
+        console.error(`exec error: ${error}`);
+        return res.status(500).send({ message: 'Error getting Terraform output' });
+      }
+  
+      try {
+        const outputs = JSON.parse(outputStdout);
+        const ip = outputs.instance_public_ip.value;
+        res.send({ message: 'EC2 Instance deployed successfully', ip: ip });
+      } catch (parseError) {
+        console.error(`Error parsing Terraform output: ${parseError}`);
+        res.status(500).send({ message: 'Error parsing Terraform output' });
+      }
+    });
   });
 });
-
-
-app.post('/api/destroyEC2', (req, res) => {
+app.post('/api/destroyWP', (req, res) => {
   exec('terraform destroy -auto-approve', { cwd: '../terraform/' }, (error, stdout, stderr) => {
     if (error) {
       console.error(`exec error: ${error}`);
       return res.status(500).send({ message: 'Error executing Terraform destroy' });
     }
     console.log(`stdout: ${stdout}`);
-    res.send({ message: 'EC2 Instance destroyed successfully' });
+    res.send({ message: 'Wordpress destroyed successfully' });
   });
 });
 
